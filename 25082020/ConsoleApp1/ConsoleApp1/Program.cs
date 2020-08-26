c49ea4a222c0d07de421ded1e14f45c20ba05ae5
Mariana <mariana.aballay@davinci.edu.ar>
2020-08-25 22:59:38 -0300

    Clase 25/08/2020

---
+++ 25082020/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroCorrecto = 56;
+            int nroCliente = 0;
+            while (numeroCorrecto != nroCliente) {
+                Console.WriteLine("Ingrese un número entre el 1 y 100. Tiene 3 intentos: ");
+                nroCliente = int.Parse(Console.ReadLine());
+                if (nroCliente == numeroCorrecto) {
+                    Console.WriteLine("Usted ha ganado el juego!");
+                }
+                else {
+                    Console.WriteLine("Seguir intentando!");
+                }
+                
+
+            }
+        }
+    }
+}
