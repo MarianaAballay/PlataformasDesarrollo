95d297aeaccde11d650a2e0d3343f1121b850cfd
Mariana <mariana.aballay@davinci.edu.ar>
2020-08-25 21:36:30 -0300

    a ver si asi si

---
+++ 18082020/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data.Common;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ident = 000;
+            Console.WriteLine("Por favor, ingrese su nombre: ");
+            String nombre = Console.ReadLine();
+            Console.WriteLine("Por favor, ingrese su nro de cliente (3 dígitos): ");
+            String numeros = Console.ReadLine();
+            ident = int.Parse(numeros);
+            String mensaje;
+
+            if (ident >= 000 & ident <=300)
+            {
+                mensaje = "Cliente Categoría Azul";
+            }
+            else if (ident >= 301 && ident <=600)
+            {
+                mensaje = "Cliente Categoría Gold";
+            }
+            else {
+                mensaje = "Cliente Categoría Platinum";
+
+            }
+            Console.WriteLine("Bienvenido " +nombre +" Eres "+ mensaje);
+        }
+    }
+}
---
+++ 18082020/ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 0;
+            Console.WriteLine("Ingrese 10 numeros");
+            for (int nro = 0; nro <= 10; nro++){
+
+                String valor = Console.ReadLine();
+                int numero = int.Parse(valor);
+                contador = contador + numero;
+
+                if (contador >= 50) {
+                    Console.WriteLine("La suma es: " + contador + " es igual o mayor a 50");
+                    break; 
+                }
+                
+            }
+
+            Console.WriteLine("La suma de los números ingresados es: " + contador);
+        }
+    }
+}
