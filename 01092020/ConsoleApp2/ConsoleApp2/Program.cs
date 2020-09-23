e5f52f436a2a0aaafa694a637fef13a8dc3e21dd
Mariana <mariana.aballay@davinci.edu.ar>
2020-09-22 22:02:20 -0300

    se agrega carpeta del 22/09/2020

---
+++ 01092020/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics.Tracing;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+      
+            Console.WriteLine(esBisiesto(IngresarAnioNac("Ingrese año de nacimiento")));
+        }
+
+        static int IngresarAnioNac(String mensaje) {
+            Console.WriteLine(mensaje);
+            String d = Console.ReadLine();
+            return int.Parse(d);
+        }
+        static bool esBisiesto(int valor) {
+            if (valor % 4 == 0 & valor % 100 != 0 & valor % 400 == 0)
+            {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+    }
+}
