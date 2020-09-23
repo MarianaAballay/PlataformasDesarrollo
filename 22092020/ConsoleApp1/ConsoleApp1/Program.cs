d4a9989c350bd8314a67e4279bfd51e62b0364f7
Mariana <mariana.aballay@davinci.edu.ar>
2020-09-22 22:57:39 -0300

    clase ok

--- 22092020/ConsoleApp1/ConsoleApp1/Program.cs
+++ 22092020/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ConsoleApp1
 {
@@ -6,7 +7,15 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            List<Tareas> listatareas = new List<Tareas>();
+            listatareas.Add(new Tareas("Ejercicio 1", "01/10/2020", 20, new Recursos(), true));
+            listatareas.Add(new Tareas("Ejercicio 2", "08/10/2020", 21, new Recursos(), true));
+            listatareas.Add(new Tareas("Ejercicio 3", "15/10/2020", 22, new Recursos(), true));
+            listatareas.Add(new Tareas("Ejercicio 4", "24/10/2020", 23, new Recursos(), true));
+            listatareas.Add(new Tareas("Ejercicio 5", "31/10/2020", 24, new Recursos(), true));
+
+
         }
     }
 }
--- 22092020/ConsoleApp1/ConsoleApp1/Tareas.cs
+++ 22092020/ConsoleApp1/ConsoleApp1/Tareas.cs
@@ -6,10 +6,21 @@ namespace ConsoleApp1
 {
     class Tareas
     {
+        public Tareas(string titulo, string vencimiento, int estimacion, Recursos responsable, Boolean estado)
+        {
+            this.titulo = titulo;
+            this.vencimiento = vencimiento;
+            this.estimacion = estimacion;
+            this.responsable = responsable;
+            this.estado = estado;
+        }
+
         public String titulo { get; set; }
         public String vencimiento { get; set; }
         public int estimacion { get; set; }
         public Recursos responsable { get; set; }
-        public String estado { get; set; }
+        public Boolean estado { get; set; }
+
+        
     }
 }
